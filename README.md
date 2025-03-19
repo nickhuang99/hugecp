da08ccdecc6bf3d0d15580b7c62480abb420ca98
nick huang <nickhuang99@hotmail.com>
2025-03-20 06:58:20 +0800

    fix stackover flow, adding a progress bar from gemini

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # hugecp
+**Hugecp a Huge Leap**
+* **What**
+Hugecp is a simple tool to **copy** your file to `[hugetlbfs](https://docs.kernel.org/admin-guide/mm/hugetlbpage.html)`
+
+* **Why**
+You want to take advantage to **preload** your model file into **memory**. In this case, it is not **mlock** in ollama. 
+Instead it uses **hugetlbfs**, a RAM-backed filesystem.
+
+* **How**
+    ```
+        1.  Make sure you setup correct kernel boot parameter to allow appropriate `**hugepagesz**`.
+        2.  Either allocate appropriate number of hugepages when boot or dynamically using sysctl.
+        3.  Mount hugetlbfs
+        4.  Now run this tool to **copy** your model file to mount point. 
+    ```
+
+* **When**
+Using this tool only when you have sufficient memory. i.e. I have no GPU, but huge memory like 1.5T
+
--- hugecp.cpp
+++ hugecp.cpp
@@ -1,4 +1,3 @@
-
 #include <sys/mman.h>
 #include <stdio.h>
 #include <errno.h>
@@ -10,9 +9,30 @@
 
 extern int errno;
 
-#define HUGE_PAGE_SIZE 2097152
+#ifndef HUGE_PAGE_SIZE
+#define HUGE_PAGE_SIZE 1073741824 //2097152
+#endif
 
-static_assert(sizeof(off_t) == 8);
+// static_assert(sizeof(off_t) == 8);
+// static_assert(HUGE_PAGE_SIZE==1073741824);
+
+static char buffer[HUGE_PAGE_SIZE];
+
+void update_progress(int progress) {
+        int bar_length = 40; // Modify this to change the bar's length
+        int filled_length = (int)(bar_length * progress / 100.0);
+        char bar[bar_length + 1]; // +1 for the null terminator
+        for (int i = 0; i < bar_length; i++) {
+                if (i < filled_length) {
+                        bar[i] = '=';
+                } else {
+                        bar[i] = '-';
+                }
+        }
+        bar[bar_length] = '\0'; // Null-terminate the string
+        printf("\r[%s] %d%%", bar, progress);
+        fflush(stdout); // Ensure output is written immediately
+}
 
 int main(int argc, char**argv)
 {        
@@ -40,8 +60,7 @@ int main(int argc, char**argv)
                 printf("target file %s cannot be opened! %s\n", argv[2], strerror(errno));
                 close(src_fd);
                 return -6;
-        }
-         
+        }         
         void* ptr = mmap(NULL, tgtSize, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_HUGETLB, tgt_fd, 0);
         
         if (ptr == MAP_FAILED) {                
@@ -50,13 +69,14 @@ int main(int argc, char**argv)
                 return -7;
         }        
         char* tgt_ptr = (char*)ptr;
-        char buffer[HUGE_PAGE_SIZE];
+        
         for (int64_t i = 0; i < pageNumber; i ++) {
                 int size;
                 if ((size = read(src_fd, buffer, HUGE_PAGE_SIZE)) == HUGE_PAGE_SIZE 
                         || (size > 0 && i == pageNumber - 1)) {
                         memcpy(tgt_ptr, buffer, size);
                         tgt_ptr += HUGE_PAGE_SIZE; // last page no need to worry
+                        update_progress(i * 100 / pageNumber);
                 } else {
                         if (size == -1) {
                                 printf("read source file %s failed with error %s\n", argv[1], strerror(errno));
@@ -68,8 +88,8 @@ int main(int argc, char**argv)
                                 break;
                         }
                 }
-        }
-        printf("copy from %s to target %s finished for file size %ld\n", argv[1], argv[2], tgtSize);
+        }        
+        printf("\ncopy from %s to target %s finished for file size %ld\n", argv[1], argv[2], tgtSize);
         munmap(ptr, tgtSize); 
         close(tgt_fd); // immediately close is better
         close(src_fd); 
